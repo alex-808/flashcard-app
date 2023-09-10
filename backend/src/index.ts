73b5d3afd40444b6610fe02f400ca3d97e1f23b9
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2023-09-10 11:13:51 -0700

    Add initial login, logout, registration endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ app.use(
         resave: false,
         saveUninitialized: false, // Only send a cookie when the session has been modified
         secret: 'SECRET',
-        store: store,
+        store: store, // Sessions are stored in the database and checked against on each request
     })
 );
 
@@ -40,16 +40,23 @@ async function main() {
 
 main().catch((err) => console.log(err));
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-});
+const userSchema = new mongoose.Schema(
+    {
+        username: String,
+        email: String,
+        password: String,
+        id: String,
+    },
+    { timestamps: true }
+);
 
 interface User {
-    name: string;
+    username: string;
     email: string;
     password: string;
+    id?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
 const userModel = mongoose.model('User', userSchema);
@@ -60,80 +67,130 @@ declare module 'express-session' {
         user: User;
     }
 }
-app.get('/', function (req, res) {
-    var body = '';
-    if (req.session.views) {
-        ++req.session.views;
-    } else {
-        req.session.views = 1;
-        body +=
-            '<p>First time visiting? view this page in several browsers :)</p>';
-    }
-    res.send(
-        body + '<p>viewed <strong>' + req.session.views + '</strong> times.</p>'
-    );
-});
 
-app.post('/register', (req: Request, res: Response) => {
-    if (!req.body.name || !req.body.email || !req.body.password) {
+app.post('/register', async (req: Request<{}, {}, User>, res: Response) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
         res.sendStatus(400);
         return;
     }
 
-    const user = {
-        name: req.body.name,
+    const existingUser = await userModel.findOne({
+        $or: [{ email: req.body.email }, { username: req.body.username }],
+    });
+    if (existingUser) {
+        const error =
+            existingUser.email === req.body.email
+                ? 'Email already exists'
+                : 'Username already exists';
+        return res.status(409).send({
+            error: error,
+        });
+    }
+
+    const registrationData: User = {
+        username: req.body.username,
         email: req.body.email,
         password: req.body.password,
     };
+
     const newUser = new userModel({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
+        username: username,
+        email: email,
+        password: password,
     });
-    newUser.save().then(() => console.log('User saved!'));
-    req.session.user = user;
+    // do we need to pull out an id here if we don't initiate a session?
+    const { _id } = await newUser.save();
+
+    if (!_id) return res.sendStatus(500);
+
+    registrationData.id = _id.toString();
+    req.session.user = registrationData;
     res.sendStatus(200);
 });
-app.get('/user', checkUser, (req: Request, res: Response) => {
-    userModel.find().then((users) => res.send(users));
-});
 
-// Requires auth
-app.post('/login', (req: Request, res: Response) => {
-    if (!req.body.email || !req.body.password) {
+app.post('/login', async (req: Request, res: Response) => {
+    if (!(req.body.username || req.body.email) || !req.body.password) {
         res.sendStatus(400);
         return;
     }
-    const user = {
-        email: req.body.email,
-        password: req.body.password,
-    };
-    userModel.findOne({ email: req.body.email }).then((user) => {
-        if (user) {
-            const existingUser = user as User;
-            if (user.password === req.body.password) {
-                req.session.user = existingUser;
-                res.sendStatus(200);
-            } else {
-                res.sendStatus(401);
-            }
-        } else {
-            res.sendStatus(401);
-        }
+
+    const user = await userModel.findOne({
+        $or: [{ email: req.body.email }, { username: req.body.username }],
     });
+
+    console.log(user);
+
+    if (!user) return res.sendStatus(401);
+
+    if (user.password !== req.body.password) return res.sendStatus(401);
+
+    if (!user._id || !user.username || !user.email || !user.password)
+        return res.sendStatus(500);
+    const sessionUser: User = {
+        username: user.username,
+        email: user.email,
+        password: user.password,
+        id: user._id.toString(),
+    };
+    // grants session
+    req.session.user = sessionUser;
+    res.sendStatus(200);
 });
+
 app.post('/logout', checkUser, (req: Request, res: Response) => {
     req.session.destroy((err) => {
         if (err) {
             console.log(err);
         }
     });
-    res.send('Logged out');
+    res.clearCookie('connect.sid');
+    res.sendStatus(200);
+});
+
+app.get('/user:userId', checkUser, async (req: Request, res: Response) => {
+    const result = await userModel.findOne({ _id: req.params.userId });
+    if (!result) return res.sendStatus(404);
+
+    if (
+        !result.username ||
+        !result.email ||
+        !result.createdAt ||
+        !result.updatedAt
+    )
+        return res.sendStatus(500);
+
+    // How do I define a User type when in some contexts it needs a password and in others it doesn't?
+    const user: User = {
+        username: result.username,
+        email: result.email,
+        createdAt: result.createdAt,
+        updatedAt: result.updatedAt,
+        password: '',
+    };
+    res.send(user);
+    // userId, username, email, created_at, updated_at
+});
+
+app.post('/user:userId/update', (req: Request, res: Response) => {});
+app.post('/user:userId/delete', checkUser, (req: Request, res: Response) => {
+    // must be logged in
+    // must be the user
+    // But what about admin users?
+    //if (req.params.userId === req.session.user.id) {
+    // console.log('delete user');
+    // }
+    // delete user
+});
+
+app.post('/generate/deck', (req: Request, res: Response) => {
+    if (!req.body.sourceContent) {
+        res.sendStatus(400);
+        return;
+    }
+    // create message and send to queue
 });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
-
-app.post('/user:userId/update', (req: Request, res: Response) => {});
-app.post('/user:userId/delete', (req: Request, res: Response) => {});
